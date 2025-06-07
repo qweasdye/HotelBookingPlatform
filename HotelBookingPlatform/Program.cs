3a18122388c8273c503fd45ecbdc3261e7ac13cd
Nikita Sukhov <n_csg_suxov@mail.ru>
2025-06-07 13:19:05 +0300

    middleware customization

--- HotelBookingPlatform/Program.cs
+++ HotelBookingPlatform/Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
 
 app.Run();
--- HotelBookingPlatform/obj/Debug/net9.0/HotelBookingPlatform.API.MvcApplicationPartsAssemblyInfo.cs
+++ HotelBookingPlatform/obj/Debug/net9.0/HotelBookingPlatform.API.MvcApplicationPartsAssemblyInfo.cs
@@ -11,13 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("HotelBookingPlatform.API")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+196150c23d0a142c4a191aa27d3df390c3bebd78")]
-[assembly: System.Reflection.AssemblyProductAttribute("HotelBookingPlatform.API")]
-[assembly: System.Reflection.AssemblyTitleAttribute("HotelBookingPlatform.API")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
 
 // Создано классом WriteCodeFragment MSBuild.
 
